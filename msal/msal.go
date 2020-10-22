44d883cd3e6eb8b8a778d7b48f9a033bfed12500
Joel Hendrix <jhendrix@microsoft.com>
2020-10-22 16:46:07 -0700

    fix references to msal in docs

--- msal/msal.go
+++ msal/msal.go
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-// Package msalgo provides the Microsoft Authentication Library (MSAL) for the Go language.
+// Package msal provides the Microsoft Authentication Library (MSAL) for the Go language.
 package msal
