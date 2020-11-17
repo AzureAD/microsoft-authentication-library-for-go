31e3c44d606b20a61f7c258ae03188d70db82dd6
Joel Hendrix <jhendrix@microsoft.com>
2020-11-17 08:56:28 -0800

    fix test to match updated error message

--- internal/msalbase/oauth_response_base_test.go
+++ internal/msalbase/oauth_response_base_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 var oauthResponse = `{}`
-var oauthResponseWithError = `{"error" : "invalid request", "error_codes" : [300]}`
+var oauthResponseWithError = `{"error" : "invalid request", "error_description": "missing payload content", "error_codes" : [300]}`
 
 func TestCreateOAuthResponseBase(t *testing.T) {
 	_, err := CreateOAuthResponseBase(404, []byte(oauthResponse))
@@ -17,7 +17,7 @@ func TestCreateOAuthResponseBase(t *testing.T) {
 	if err.Error() != actualError.Error() {
 		t.Errorf("Actual error %v differs from expected error %v", err, actualError)
 	}
-	expectedErr := errors.New("invalid request")
+	expectedErr := errors.New("invalid request: missing payload content")
 	_, err = CreateOAuthResponseBase(300, []byte(oauthResponseWithError))
 	if err.Error() != expectedErr.Error() {
 		t.Errorf("Actual error %v differs from expected error %v", err, expectedErr)
