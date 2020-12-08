9119809f4ca299eb0d971acb4fa665fb066b0e96
John Doak <jdoak@DANEWMAN-LENOVO.redmond.corp.microsoft.com>
2020-12-08 11:41:51 -0800

    fix comment

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -317,7 +317,7 @@ func (m *Manager) readAccount(homeAccountID string, envAliases []string, realm s
 	// We only use a map because the storage contract shared between all language implementations says use a map.
 	// We can't change that. The other is because the keys are made using a specific "env", but here we are allowing
 	// a match in multiple envs (envAlias). That means we either need to hash each possible keyand do the lookup
-	// or just statically check.  Since the design is to have a storage.Manager per use, the amount of keys stored
+	// or just statically check.  Since the design is to have a storage.Manager per user, the amount of keys stored
 	// is really low (say 2).  Each hash is more expensive than the entire iteration.
 	for _, acc := range cache.Accounts {
 		if acc.HomeAccountID == homeAccountID && checkAlias(acc.Environment, envAliases) && acc.Realm == realm {
