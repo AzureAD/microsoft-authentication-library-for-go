28324c6ed3f940e3078b62044f241f7c6ca72268
John Doak <jdoak@DANEWMAN-LENOVO.redmond.corp.microsoft.com>
2020-12-18 13:50:54 -0800

    lint

--- internal/ops/internal/comm/compress.go
+++ internal/ops/internal/comm/compress.go
@@ -10,14 +10,18 @@ func gzipDecompress(r io.Reader) io.Reader {
 
 	pipeOut, pipeIn := io.Pipe()
 	go func() {
-		_, err := io.Copy(pipeIn, gzipReader)
+		// decompression bomb would have to come from Azure services.
+		// If we want to limit, we should do that in comm.do().
+		_, err := io.Copy(pipeIn, gzipReader) //nolint
 		if err != nil {
-			pipeIn.CloseWithError(err)
+			// don't need the error.
+			pipeIn.CloseWithError(err) //nolint
 			gzipReader.Close()
 			return
 		}
 		if err := gzipReader.Close(); err != nil {
-			pipeIn.CloseWithError(err)
+			// don't need the error.
+			pipeIn.CloseWithError(err) //nolint
 			return
 		}
 		pipeIn.Close()
