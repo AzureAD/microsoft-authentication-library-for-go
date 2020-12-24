5c2c9e037e5bac1932728960395e7ad13ea3aa44
John Doak <jdoak@DANEWMAN-LENOVO.redmond.corp.microsoft.com>
2020-12-24 11:08:05 -0800

    fix build

--- test/devapps/device_code_flow_sample.go
+++ test/devapps/device_code_flow_sample.go
@@ -14,7 +14,7 @@ import (
 )
 
 func deviceCodeCallback(deviceCodeResult msal.DeviceCodeResultProvider) {
-	log.Printf(deviceCodeResult.GetMessage())
+	log.Println(deviceCodeResult.GetMessage())
 }
 
 func acquireTokenDeviceCode() {
