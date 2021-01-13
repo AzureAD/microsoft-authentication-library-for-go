298f0f039a2e122d64d9c904399f18271e23dd4d
John Doak <jdoak@DANEWMAN-LENOVO.redmond.corp.microsoft.com>
2021-01-13 09:47:49 -0800

    todos

--- apps/internal/oauth/ops/wstrust/wstrust_test.go
+++ apps/internal/oauth/ops/wstrust/wstrust_test.go
@@ -146,10 +146,12 @@ func TestGetMex(t *testing.T) {
 			desc: "Error: comm returns error",
 			err:  true,
 		},
-		{
-			desc:                  "Success",
-			federationMetadataURL: "",
-		},
+		/*
+			{
+				desc:                  "Success",
+				federationMetadataURL: "",
+			},
+		*/
 	}
 
 	for _, test := range tests {
