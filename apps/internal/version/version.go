4da9baa33271a186ca8c2b933e7e5835d3c3d76b
Abhidnya <patil.abhidnya@gmail.com>
2021-03-29 14:22:28 -0700

    Adding version file

---
+++ apps/internal/version/version.go
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+package version
+
+// Version is the version of this client package that is communicated to the server.
+const Version = ""
