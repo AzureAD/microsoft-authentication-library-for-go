a4323f8f6b1d9d8566c0383590dcb3b6da76b064
Abhidnya <patil.abhidnya@gmail.com>
2021-03-29 15:56:46 -0700

    Adding comment

--- apps/internal/version/version.go
+++ apps/internal/version/version.go
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+// Package version keeps the version number of the client package.
 package version
 
 // Version is the version of this client package that is communicated to the server.
