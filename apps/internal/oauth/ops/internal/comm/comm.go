2ec26d512729b21dfb8a32503bb3e90c611e41f7
Abhidnya <patil.abhidnya@gmail.com>
2021-03-29 16:01:34 -0700

    Adding version file (#201)

--- apps/internal/oauth/ops/internal/comm/comm.go
+++ apps/internal/oauth/ops/internal/comm/comm.go
@@ -21,10 +21,9 @@ import (
 
 	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/errors"
 	customJSON "github.com/AzureAD/microsoft-authentication-library-for-go/apps/internal/json"
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/internal/version"
 	"github.com/google/uuid"
 )
-
-const version = "0.1.0"
 
 // HTTPClient represents an HTTP client.
 // It's usually an *http.Client from the standard library.
@@ -317,6 +316,6 @@ func addStdHeaders(headers http.Header) http.Header {
 	headers.Set("x-client-sku", "MSAL.Go")
 	headers.Set("x-client-os", runtime.GOOS)
 	headers.Set("x-client-cpu", runtime.GOARCH)
-	headers.Set("x-client-ver", version)
+	headers.Set("x-client-ver", version.Version)
 	return headers
 }
