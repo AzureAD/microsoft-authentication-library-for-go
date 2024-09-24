17d2ef189c20f0b684324b5cdd12a055ded7d836
Andrew O Hart <andrewohart@microsoft.com>
2024-09-24 17:44:30 +0100

    Change pem file message

--- apps/tests/integration/integration_test.go
+++ apps/tests/integration/integration_test.go
@@ -36,7 +36,7 @@ const (
 
 	// Default values
 	defaultClientId = "f62c5ae3-bf3a-4af5-afa8-a68b800396e9"
-	pemFile         = "/Users/andywork/Downloads/file.pem"
+	pemFile         = "Insert path to pem file here"
 )
 
 var httpClient = http.Client{}
