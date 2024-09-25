6b9cd68c0fb828dc8aa02a91e2e3388534e54623
Nilesh Choudhary <nichoudhary@microsoft.com>
2024-09-25 09:00:15 +0100

    Removed some tests which were redundant

--- apps/tests/devapps/main.go
+++ apps/tests/devapps/main.go
@@ -36,7 +36,7 @@ func main() {
 	} else if exampleType == "6" {
 		// This sample does not use a serialized cache - it relies on in-memory cache by reusing the app object
 		// This works well for app tokens, because there is only 1 token per resource, per tenant.
-		// acquireTokenClientCertificate()
+		acquireTokenClientCertificate()
 		// // this time the token comes from the cache!
 		// acquireTokenClientCertificate()
 	} else if exampleType == "7" {
