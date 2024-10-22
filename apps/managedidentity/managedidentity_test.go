0f9c2f389a199b8b5502c65905a8c267d0e65de0
Andrew O Hart <andrewohart@microsoft.com>
2024-10-22 16:41:12 +0100

    test fix

--- apps/managedidentity/managedidentity_test.go
+++ apps/managedidentity/managedidentity_test.go
@@ -738,7 +738,7 @@ func TestHandleAzureArcResponse(t *testing.T) {
 			name:           "Invalid file path",
 			statusCode:     http.StatusUnauthorized,
 			headers:        map[string]string{wwwAuthenticateHeaderName: "Basic realm=/path/to/secret.key"},
-			expectedError:  "invalid file path, expected /path/to/secret.key, got " + filepath.Dir(testCaseFilePath),
+			expectedError:  "invalid file path, expected " + filepath.Dir(testCaseFilePath) + "got /path/to/secret.key",
 			platform:       runtime.GOOS,
 			createMockFile: true,
 		},
