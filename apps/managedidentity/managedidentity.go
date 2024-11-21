1cd9908f2027a20fa10408243fb233ae41fb619e
Nilesh Choudhary <107404295+4gust@users.noreply.github.com>
2024-11-21 17:56:17 +0000

    Update apps/managedidentity/managedidentity.go
    Co-authored-by: Charles Lowell <10964656+chlowell@users.noreply.github.com>

--- apps/managedidentity/managedidentity.go
+++ apps/managedidentity/managedidentity.go
@@ -102,6 +102,7 @@ var retryStatusCodes = []int{
 	http.StatusRequestTimeout,      // 408
 	http.StatusTooManyRequests,     // 429
 	http.StatusInternalServerError, // 500
+	http.StatusBadGateway,          // 502
 	http.StatusServiceUnavailable,  // 503
 	http.StatusGatewayTimeout,      // 504
 }
