fca7a8a03251e8faf1288b688a306856507feb06
Andrew O Hart <andrewohart@microsoft.com>
2025-01-09 22:48:37 +0000

    * update documentation

--- apps/confidential/confidential_121_plus.go
+++ apps/confidential/confidential_121_plus.go
@@ -6,12 +6,9 @@ import (
 	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/internal/slog"
 )
 
-// WithLogger allows for a custom slog logger to be set
-// More information on slog here: https://pkg.go.dev/log/slog
-// This can be used with confidential client when creating a New instance similar to this:
-// customSlogLogger := slog.Default()
-// confidentialClient, err := New(authority, clientID, cred, WithLogger(customSlogLogger))
-// Just to note that WithLogger(nil) == WithLogger(slog.Default())
+// WithLogger enables logging within the SDK
+// When l is nil, client will use slog.Default()
+// Panic will occur if slog.Default() returns nil
 func WithLogger(l *slog.Logger) Option {
 	return func(o *clientOptions) {
 		o.logger = l
