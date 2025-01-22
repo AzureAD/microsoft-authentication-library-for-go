0ef46fe44a002281fed969196ec283a56a31a318
Andrew O Hart <andrewohart@microsoft.com>
2025-01-22 19:24:47 +0000

    Update documentation WithLogger

--- apps/managedidentity/with_logger.go
+++ apps/managedidentity/with_logger.go
@@ -7,8 +7,6 @@ import (
 )
 
 // WithLogger enables logging within the SDK
-// When l is nil, client will use slog.Default()
-// Panic will occur if slog.Default() returns nil
 func WithLogger(l *slog.Logger) ClientOption {
 	return func(o *ClientOptions) {
 		o.logger = l
